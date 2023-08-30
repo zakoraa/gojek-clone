068ed5fbe2ed5f48d39b91a6df71965b2b10b6a2
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-08-30 22:47:43 +0700

    [ADD] coming soon page

---
+++ lib/coming_soon.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ComingSoon extends StatelessWidget {
+  const ComingSoon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Coming Soon",style: TextStyle(
+        fontSize: 30
+        ),),
+      ),
+    );
+  }
+}
