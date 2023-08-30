0dd007d911fd2b6c5d8c6691426a9f0624564c3a
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-08-30 22:44:44 +0700

    [ADD] Custom Color

---
+++ lib/shared/theme/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class CustomColor {
+  static const darkGreen = Color(0xFF00880F);
+  static const green = Color(0xFF00AA12);
+  static const ligthGreen = Color.fromARGB(255, 0, 202, 20);
+  static const lightGrey = Color.fromARGB(255, 243, 243, 243);
+}
