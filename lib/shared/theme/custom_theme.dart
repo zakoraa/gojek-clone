0a42969d554b06667fd2f2c6d0332853c87976f8
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-08-30 22:49:44 +0700

    [ADD] scaffold background in custom theme

--- lib/shared/theme/custom_theme.dart
+++ lib/shared/theme/custom_theme.dart
@@ -3,8 +3,9 @@ import 'package:google_fonts/google_fonts.dart';
 
 abstract class CustomTheme {
   static var theme1 = ThemeData(
+      scaffoldBackgroundColor: Colors.white,
       textTheme: TextTheme(
           bodyMedium: GoogleFonts.poppins(
-    fontSize: 16,
-  )));
+        fontSize: 16,
+      )));
 }
