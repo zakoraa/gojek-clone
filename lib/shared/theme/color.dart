1008a41a3663058bfd1b6aa21888e45a48248735
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-08-31 17:45:57 +0700

    [UPDATE] theme colors

--- lib/shared/theme/color.dart
+++ lib/shared/theme/color.dart
@@ -4,6 +4,6 @@ abstract class CustomColor {
   static const darkGreen = Color(0xFF00880F);
   static const green = Color(0xFF00AA12);
   static const ligthGreen = Color.fromARGB(255, 0, 202, 20);
-  static const lightGrey = Color.fromARGB(255, 243, 243, 243);
+  static const lightGrey = Color.fromARGB(255, 246, 245, 245);
   static const grey = Color.fromARGB(255, 206, 205, 205);
 }
