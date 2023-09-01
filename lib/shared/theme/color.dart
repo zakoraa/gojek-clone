8f63791988c525907f9eae72819523490ae1782d
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-09-01 17:58:14 +0700

    [ADD] box shadow color

--- lib/shared/theme/color.dart
+++ lib/shared/theme/color.dart
@@ -6,4 +6,5 @@ abstract class CustomColor {
   static const ligthGreen = Color.fromARGB(255, 0, 202, 20);
   static const lightGrey = Color.fromARGB(255, 246, 245, 245);
   static const grey = Color.fromARGB(255, 206, 205, 205);
+  static final boxShadow = Colors.grey.withOpacity(0.4);
 }
