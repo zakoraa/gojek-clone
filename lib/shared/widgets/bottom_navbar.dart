81022017bb52baef69fbed79e322df3aa1c71480
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-11-05 17:09:48 +0700

    fix: bottom navbar

--- lib/shared/widgets/bottom_navbar.dart
+++ lib/shared/widgets/bottom_navbar.dart
@@ -15,100 +15,102 @@ class BottomNavbar extends StatelessWidget {
       "Pesanan": Icons.shopping_bag_rounded,
       "Chat": Icons.chat
     };
+
+    double tabWidth =
+        MediaQuery.of(context).size.width / bottomNavbar.length - 5;
     return Positioned(
         bottom: 0,
         left: 0,
         child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          height: 70,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 10,
-                blurRadius: 10,
-                offset: const Offset(0, 5),
-              ),
-            ],
-          ),
-          child: Center(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: List.generate(
-                bottomNavbar.length,
-                (index) => BlocBuilder<BuildPageCubit, BottomNavBarTab>(
-                    builder: (context, state) {
-                  bool isSelectedPage = state == BottomNavBarTab.values[index];
-                  return GestureDetector(
-                    onTap: () => context
-                        .read<BuildPageCubit>()
-                        .selectPage(BottomNavBarTab.values[index]),
-                    child: AnimatedContainer(
-                      duration: const Duration(milliseconds: 500),
-                      curve: Curves.easeOut,
-                      height: 70,
-                      width: 80,
-                      decoration: isSelectedPage
-                          ? const BoxDecoration(
-                              gradient: LinearGradient(
-                                  begin: Alignment.topCenter,
-                                  end: Alignment.bottomCenter,
-                                  colors: [
-                                  Color.fromARGB(5, 0, 255, 8),
-                                  Color.fromARGB(106, 255, 255, 255),
-                                  Color.fromARGB(87, 255, 255, 255),
-                                ]))
-                          : const BoxDecoration(
-                              color: Colors.white,
-                            ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          AnimatedContainer(
-                            duration: const Duration(milliseconds: 500),
-                            curve: Curves.easeOut,
-                            height: 5,
-                            width: isSelectedPage ? 80 : 0,
-                            decoration: const BoxDecoration(
-                                color: CustomColor.darkGreen,
-                                borderRadius: BorderRadius.only(
-                                    bottomLeft: Radius.circular(20),
-                                    bottomRight: Radius.circular(20))),
-                          ),
-                          const SizedBox(
-                            height: 10.0,
-                          ),
-                          Icon(
-                            bottomNavbar.values.elementAt(index),
-                            color: isSelectedPage
-                                ? CustomColor.darkGreen
-                                : const Color.fromARGB(255, 138, 138, 138),
-                            size: 25,
-                          ),
-                          const SizedBox(
-                            height: 5.0,
-                          ),
-                          AutoSizeText(
-                            bottomNavbar.keys.elementAt(index),
-                            minFontSize: 8,
-                            maxLines: 1,
-                            style: TextStyle(
-                                fontSize: 10,
-                                color: state == BottomNavBarTab.values[index]
-                                    ? CustomColor.darkGreen
-                                    : Colors.black),
-                          )
-                        ],
-                      ),
-                    ),
-                  );
-                }),
-              ),
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            height: 70,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 10,
+                  blurRadius: 10,
+                  offset: const Offset(0, 5),
+                ),
+              ],
             ),
-          ),
-        ));
+            child: Stack(fit: StackFit.expand, children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: List.generate(
+                  bottomNavbar.length,
+                  (index) => BlocBuilder<BuildPageCubit, BottomNavBarTab>(
+                      builder: (context, state) {
+                    bool isSelectedPage =
+                        state == BottomNavBarTab.values[index];
+                    return GestureDetector(
+                      onTap: () => context
+                          .read<BuildPageCubit>()
+                          .selectPage(BottomNavBarTab.values[index]),
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.easeOut,
+                        height: 70,
+                        width: tabWidth,
+                        decoration: isSelectedPage
+                            ? const BoxDecoration(
+                                gradient: LinearGradient(
+                                    begin: Alignment.topCenter,
+                                    end: Alignment.bottomCenter,
+                                    colors: [
+                                    Color.fromARGB(5, 0, 255, 8),
+                                    Color.fromARGB(106, 255, 255, 255),
+                                    Color.fromARGB(87, 255, 255, 255),
+                                  ]))
+                            : const BoxDecoration(
+                                color: Colors.white,
+                              ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              bottomNavbar.values.elementAt(index),
+                              color: isSelectedPage
+                                  ? CustomColor.darkGreen
+                                  : const Color.fromARGB(255, 138, 138, 138),
+                              size: 25,
+                            ),
+                            const SizedBox(
+                              height: 5.0,
+                            ),
+                            AutoSizeText(
+                              bottomNavbar.keys.elementAt(index),
+                              minFontSize: 8,
+                              maxLines: 1,
+                              style: TextStyle(
+                                  fontSize: 10,
+                                  color: state == BottomNavBarTab.values[index]
+                                      ? CustomColor.darkGreen
+                                      : Colors.black),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+              ),
+              Positioned(
+                top: 0,
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeOut,
+                  height: 5,
+                  width: tabWidth,
+                  decoration: const BoxDecoration(
+                      color: CustomColor.darkGreen,
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(20))),
+                ),
+              )
+            ])));
   }
 }
